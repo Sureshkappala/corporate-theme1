795a6f8c8eb8eaf2047379ad5ab43a0ca0724162
Sureshkappala <144470888+Sureshkappala@users.noreply.github.com>
2026-06-10 11:20:09 +0530

    Fixed tester issues, added animations, dashboard enhancements, and responsive design

--- js/script.js
+++ js/script.js
@@ -64,3 +64,32 @@ function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("active");
     document.body.classList.toggle("menu-open");
 }
+const registerForm = document.getElementById("registerForm");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        alert("Successfully Registered");
+
+        window.location.href = "login.html";
+    });
+}
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    let count = 0;
+    const target = parseInt(counter.innerText);
+
+    const updateCounter = () => {
+        if(count < target){
+            count += Math.ceil(target / 50);
+            counter.innerText = count;
+            setTimeout(updateCounter, 30);
+        } else {
+            counter.innerText = target + "+";
+        }
+    };
+
+    updateCounter();
+});
